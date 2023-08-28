5e8c4caff3fbb8d64885b5064cba4ffba05a9251
Tifa Software <83831269+tifasoftware@users.noreply.github.com>
2023-08-28 15:56:29 -0500

    Delete HP-UX directory

--- HP-UX/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-qemu-system-hppa -m 512 -machine hppa -drive if=scsi,bus=0,index=6,file=HP-UX_hdd0.qcow2 -cdrom /Users/jesse/Downloads/HP-UX\ 10.20\ \[HP9000\ S700\]/hpux-10.20_os_700.iso -net nic,model=tulip -net user
